48b57fdbef21d7cb793275b3896015d4e70135a4
yuriykolomiyets <juriy121@gmail.com>
2022-08-30 12:27:17 +0200

    141. Assignment: Display List of Recipes on Index Page

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,16 +17,19 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String directions;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @Lob
     private Byte[] image;
 
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
+
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -34,7 +38,7 @@ public class Recipe {
     @JoinTable(name = "recipe_category",
         joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
 
     public Long getId() {
         return id;
--- src/main/java/guru/springframework/repositories/RecipeRepository.java
+++ src/main/java/guru/springframework/repositories/RecipeRepository.java
@@ -3,5 +3,8 @@ package guru.springframework.repositories;
 import guru.springframework.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+  //  Optional<Recipe> findByDescription(String description);
 }
